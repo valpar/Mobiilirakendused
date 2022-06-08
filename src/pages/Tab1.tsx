08efc22e13872529ab92a44236fe01501bc3f026
valpar <valmarparts@gmail.com>
2022-06-08 23:11:43 +0300

    Arvestustöö  baas

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,55 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React, { useState } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCheckbox,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonItemDivider,
+} from '@ionic/react';
 
-const Tab1: React.FC = () => {
+const checkboxList = [
+  { val: 'Voodi tegemine', isChecked: false },
+  { val: 'Hambapesu', isChecked: false },
+  { val: 'Duši all käimine', isChecked: false },
+  { val: 'Hommikusöök', isChecked: false },
+  { val: 'Tööle/kooli minek', isChecked: false },
+  { val: 'Töötamine/õppimine', isChecked: false },
+  { val: 'Lõuna', isChecked: false },
+  { val: 'Töötamine/õppimine', isChecked: false },
+  { val: 'Koju minek', isChecked: false },
+  { val: 'Kodutööd', isChecked: false },
+  { val: 'Hobid', isChecked: false },
+  { val: 'Hambapesu', isChecked: false },
+  { val: 'Magama minek', isChecked: false },
+];
+
+export const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Daily to-do list</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent>
+        <IonList>
+          {checkboxList.map(({ val, isChecked }, i) => (
+            <IonItem key={i}>
+              <IonLabel>{val}</IonLabel>
+              <IonCheckbox
+                color='success'
+                slot='end'
+                value={val}
+                checked={isChecked}
+              />
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
